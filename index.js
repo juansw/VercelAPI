fab250da7f249d5cc21d2d61f9d367be8e3578cb
Juan Guajardo <gigfmedia@icloud.com>
2025-02-17 11:25:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 import express from "express";
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 import path from "path";
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, Vercel!');
 });
 
 app.post('/api', async (req, res) => {
-  //res.json({ message: 'This is an API endpoint' });
   try {
     const apiResponse = await fetch("https://apim.workato.com/api_ferco/searchi-v1/search");
     const data = await apiResponse.json();
     res.json(data);
   } catch (error) {
-      console.error("Error fetching data:", error);
-      res.status(500).json({ error: "Error fetching data", details: error.message });
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Error fetching data", details: error.message });
   }
 });
 
