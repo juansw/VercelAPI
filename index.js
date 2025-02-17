efbdc02701273bbbf3114a5334b003cd142bc908
Juan Guajardo <gigfmedia@icloud.com>
2025-02-17 10:43:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,25 +7,16 @@ app.get('/', (req, res) => {
   res.send('Hello, Vercel!');
 });
 
-app.get('/api', (req, res) => {
+app.post('/api', async (req, res) => {
   //res.json({ message: 'This is an API endpoint' });
   try {
-        const apiResponse = await fetch('https://apim.workato.com/api_ferco/searchi-v1/search', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'api-token': '94700300d77ce83c0806ec2e9cec6d16f24522b9a49bf4e9638d7efcc1e39710'
-            },
-            //body: JSON.stringify(req.body),
-            body: JSON.stringify(payload),
-        });
-
-        const data = await apiResponse.text(); // o .json() si la respuesta es JSON
-        res.send(data);
-    } catch (error) {
-        console.error('Error en el proxy:', error);
-        res.status(500).send('Error al obtener la API');
-    }
+    const apiResponse = await fetch("https://apim.workato.com/api_ferco/searchi-v1/search");
+    const data = await apiResponse.json();
+    res.json(data);
+  } catch (error) {
+      console.error("Error fetching data:", error);
+      res.status(500).json({ error: "Error fetching data", details: error.message });
+  }
 });
 
 // Export the Express app for Vercel to use
